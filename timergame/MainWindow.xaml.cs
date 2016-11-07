b60574172b5432b7ff59c56ce87b5a849ee12781
liam.brangers <liam.brangers@immalle.be>
2016-11-07 11:56:39 +0100

    Start of timergame

--- timergame/MainWindow.xaml.cs
+++ timergame/MainWindow.xaml.cs
@@ -12,17 +12,40 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace timergame
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        private DispatcherTimer spawntimer = new DispatcherTimer();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            spawntimer.Interval = TimeSpan.FromMilliseconds(200);
+            //spawntimer.Tick += Spawntimer_Tick;
+        }
+        
+
+        private void Spawntimer_tick()
+        {
+            vuller.Height = +10;
+            vuller.Width = +10;
+            
+        }
+
+        private void start_Click(object sender, RoutedEventArgs e)
+        {
+            spawntimer.Start();
+        }
+
+        private void tinier_Click(object sender, RoutedEventArgs e)
+        {
+            vuller.Height = -5;
+            vuller.Width = -5;
         }
     }
 }
